810ccbd00aa7134e24aebddebcf2f4b9673f3c9d
Tan Sy <sytanta@yahoo.com>
2020-02-06 00:28:28 +0700

    Highlight code using prismjs

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,16 @@
 const React = require("react")
 
-exports.onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([<script src="https://d3js.org/d3.v5.min.js"></script>])
+exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  setHeadComponents([
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.19.0/themes/prism-coy.min.css"
+      rel="stylesheet"
+    />,
+    <script src="https://d3js.org/d3.v5.min.js"></script>,
+  ])
+
+  setPostBodyComponents([
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.19.0/components/prism-core.min.js" data-manual></script>,
+    <script src="/libs/prism-highlight.js"></script>,
+  ])
 }
