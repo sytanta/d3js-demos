6a1f4a109ae79b6ae34dd7ae317bf6067211b995
Tan Sy <sytanta@yahoo.com>
2020-02-08 10:38:56 +0700

    Redirect to home when accessing undefined routes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,32 +1,12 @@
-import React from "react"
-import { graphql } from "gatsby"
+import { useEffect } from "react"
+import { navigate } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+const ReDirect = () => {
+  useEffect(() => {
+    navigate("")
+  }, [])
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+  return null
 }
 
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default ReDirect
