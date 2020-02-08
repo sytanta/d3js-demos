ce38a37eed44ce75c1e872cfd28780e6f98bebd5
Tan Sy <sytanta@yahoo.com>
2020-02-08 10:38:13 +0700

    Add linkedin contact icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,9 @@
 import React from "react"
 
-const Footer = ({ title }) => (
-  <footer style={{ display: "flex", justifyContent: "space-between", padding: "2rem 1rem" }}>
+import classes from "../styles/footer.module.css"
+
+const Footer = () => (
+  <footer className={classes.footer}>
     <div>
       © {new Date().getFullYear()}, Built with
       {` `}
@@ -9,7 +11,15 @@ const Footer = ({ title }) => (
       {` `}
       <a href="https://d3js.org/">D3.js</a>
     </div>
-    <div></div>
+    <div>
+      <a
+        href="https://www.linkedin.com/in/tan-sy-688b8a87/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img width="20" src="images/linkedin.png" alt="Contact" />
+      </a>
+    </div>
   </footer>
 )
 
